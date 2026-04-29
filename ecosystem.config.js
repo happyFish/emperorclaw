d0d6e9ae22eaff585a7d638b2624616f0d241402
Jose Zuma <jose.zunma@gmail.com>
2026-04-30 00:00:01 +0200

    chore: implement zero downtime blue/green deployments

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [
+    {
+      name: "emperorclaw",
+      script: "node_modules/next/dist/bin/next",
+      args: "start",
+      instances: 1, // Run a single instance in cluster mode (allows zero-downtime reload)
+      exec_mode: "cluster",
+      watch: false,
+      max_memory_restart: "1G",
+      env: {
+        NODE_ENV: "production",
+        PORT: 3000
+      }
+    }
+  ]
+};
